2d029a9fac03a09bd79ab1925859eefdb5fd54c6
Jan <jan.kolarik.18@ucl.ac.uk>
2020-01-13 12:27:39 +0000

    Gyroscope and Accellerometer data gathered

--- Apple Watch Code/DanceHealthTrack/DanceHealthTrack WatchKit Extension/danceTracking.swift
+++
@@ -1,9 +0,0 @@
-//
-//  danceTracking.swift
-//  DanceHealthTrack WatchKit Extension
-//
-//  Created by Honza on 07/12/2019.
-//  Copyright © 2019 Team32. All rights reserved.
-//
-
-import Foundation
