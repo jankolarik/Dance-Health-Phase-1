bebe4d84a54d8efdf300758c2d2245497b6e108a
Alex Tcherdakoff <zcabnap@ucl.ac.uk>
2020-01-15 00:25:59 +0100

    this doesn't work yet

--- Kinect/SkeletalTracking/SkeletalTracking.cpp
+++ Kinect/SkeletalTracking/SkeletalTracking.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <cmath>
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+#include <d2d1.h>
+
+//reference: sample BodyBasics-D2D from Kinect SDK
 
 // No need to include the std keyword before cout
 using namespace std;
@@ -18,6 +21,11 @@ inline void SafeRelease(Interface*& pInterfaceToRelease)
 	}
 }
 
+static const float c_JointThickness = 3.0f;
+static const float c_TrackedBoneThickness = 6.0f;
+static const float c_InferredBoneThickness = 1.0f;
+static const float c_HandSize = 30.0f;
+
 
 class SkeletalBasics
 {
@@ -28,7 +36,7 @@ public:
 	// Destructor
 	~SkeletalBasics();
 
-	int Run();
+	int Run(HINSTANCE hInstance, int nCmdShow);
 
 private:
 	// Current Kinect
@@ -46,12 +54,36 @@ private:
 	float				 m_fCalibrationValue;
 	clock_t				 m_nCalibrationStartTime;
 
+	// window handle
+	HWND				 m_hWnd;
+
+	//d2d1 vals
+
+	ID2D1Factory*		 m_pD2DFactory;
+	ID2D1RenderTarget*   m_pRenderTarget;
+	ID2D1Brush*		 	 m_pBrushJointTracked;
+	ID2D1Brush*		 	 m_pBrushJointInferred;
+	ID2D1Brush*		 	 m_pBrushBoneTracked;
+	ID2D1Brush*			 m_pBrushBoneInferred;
+	ID2D1Brush*			 m_pBrushHandClosed;
+	ID2D1Brush*			 m_pBrushHandOpen;
+	ID2D1Brush*			 m_pBrushHandLasso;
+
+	// Time Vals
+
+	INT64 m_nStartTime;
+	int	m_nLastCounter;
+	int	m_nFramesSinceUpdate;
+	int	m_fFreq;
+	int	m_nNextStatusTime;
+
+	// functs
 
 	void				 Update();
 
 	HRESULT				 InitializeDefaultSensor();
 
-	void				 ProcessBody(int nBodyCount, IBody** ppBodies);
+	void				 ProcessBody(INT64 nTime, int nBodyCount, IBody** ppBodies);
 
 	float				 ActivityAnalysis(IBody* pBodyFromCurrentFrame, IBody* pBodyFromPreviousFrame);
 
@@ -60,6 +92,26 @@ private:
 	void				 Calibration(IBody** ppBodies);
 
 	IBody*				 getSingleBody(IBody** ppBodies);
+
+	LRESULT CALLBACK	 MessageRouter(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	LRESULT CALLBACK	 DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+	bool				 SetStatusMessage(_In_z_ WCHAR* szMessage, DWORD nShowTimeMsec, bool bForce);
+
+	void				 DiscardDirect2DResources();
+
+	HRESULT				 EnsureDirect2DResources();
+
+	D2D1_POINT_2F		 BodyToScreen(const CameraSpacePoint& bodyPoint, int width, int height);
+
+	void				 DrawBody(const Joint* pJoints, const D2D1_POINT_2F* pJointPoints);
+
+	void				 DrawBone(const Joint* pJoints, const D2D1_POINT_2F* pJointPoints, JointType joint0, JointType joint1);
+
+	void				 DrawHand(HandState handState, const D2D1_POINT_2F& handPosition);
+
+
 };
 
 
@@ -69,34 +121,163 @@ SkeletalBasics::SkeletalBasics() :
 	m_pBodyFrameReader(NULL),
 	m_bCalibrationStatus(false),
 	m_fCalibrationValue(0),
-	m_nCalibrationStartTime(0)
+	m_nCalibrationStartTime(0),
+	m_pD2DFactory(NULL),
+	m_pRenderTarget(NULL),
+	m_pBrushJointTracked(NULL),
+	m_pBrushJointInferred(NULL),
+	m_pBrushBoneTracked(NULL),
+	m_pBrushBoneInferred(NULL),
+	m_pBrushHandClosed(NULL),
+	m_pBrushHandOpen(NULL),
+	m_pBrushHandLasso(NULL),
+	m_hWnd(NULL),
+	m_nStartTime(0),
+	m_nLastCounter(0),
+	m_nFramesSinceUpdate(0),
+	m_fFreq(0),
+	m_nNextStatusTime(0LL)
 {
 }
 
 
 SkeletalBasics::~SkeletalBasics()
 {
+	SafeRelease(m_pRenderTarget);
+
+	SafeRelease(m_pBrushJointTracked);
+	SafeRelease(m_pBrushJointInferred);
+	SafeRelease(m_pBrushBoneTracked);
+	SafeRelease(m_pBrushBoneInferred);
+
+	SafeRelease(m_pBrushHandClosed);
+	SafeRelease(m_pBrushHandOpen);
+	SafeRelease(m_pBrushHandLasso);
+
+	// clean up Direct2D
+	SafeRelease(m_pD2DFactory);
+
+	// done with body frame reader
+	SafeRelease(m_pBodyFrameReader);
+
+	// done with coordinate mapper
+	SafeRelease(m_pCoordinateMapper);
+
 	// close the Kinect Sensor
 	if (m_pKinectSensor)
 	{
 		m_pKinectSensor->Close();
 	}
+
+	SafeRelease(m_pKinectSensor);
 }
 
 
-int SkeletalBasics::Run()
+int SkeletalBasics::Run(HINSTANCE hInstance, int nCmdShow)
 {
+	MSG       msg = { 0 };
+	WNDCLASS  wc;
+
+	// Dialog custom window class
+	ZeroMemory(&wc, sizeof(wc));
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.cbWndExtra = DLGWINDOWEXTRA;
+	wc.hCursor = LoadCursorW(NULL, IDC_ARROW);
+	wc.hIcon = LoadIconW(hInstance, MAKEINTRESOURCE(IDI_APP));
+	wc.lpfnWndProc = DefDlgProcW;
+	wc.lpszClassName = L"BodyBasicsAppDlgWndClass";
+
+	if (!RegisterClassW(&wc))
+	{
+		return 0;
+	}
+
+	// Create main application window
+	HWND hWndApp = CreateDialogParamW(
+		NULL,
+		MAKEINTRESOURCE(IDD_APP),
+		NULL,
+		(DLGPROC)SkeletalBasics::MessageRouter,
+		reinterpret_cast<LPARAM>(this));
+
+	// Show window
+	ShowWindow(hWndApp, nCmdShow);
 	// Initialize Kinect Sensor
 	InitializeDefaultSensor();
 
-	while (true)
+	while (WM_QUIT != msg.message)
 	{
-		// Keep updating data from sensor to the program
 		Update();
+
+		while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			// If a dialog message will be taken care of by the dialog proc
+			if (hWndApp && IsDialogMessageW(hWndApp, &msg))
+			{
+				continue;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+		}
 	}
+}
+
+LRESULT CALLBACK SkeletalBasics::MessageRouter(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	SkeletalBasics* pThis = NULL;
+
+	if (WM_INITDIALOG == uMsg)
+	{
+		pThis = reinterpret_cast<SkeletalBasics*>(lParam);
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
+	}
+	else
+	{
+		pThis = reinterpret_cast<SkeletalBasics*>(::GetWindowLongPtr(hWnd, GWLP_USERDATA));
+	}
+
+	if (pThis)
+	{
+		return pThis->DlgProc(hWnd, uMsg, wParam, lParam);
+	}
+
 	return 0;
 }
 
+LRESULT CALLBACK SkeletalBasics::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(wParam);
+	UNREFERENCED_PARAMETER(lParam);
+
+	switch (message)
+	{
+	case WM_INITDIALOG:
+	{
+		// Bind application window handle
+		m_hWnd = hWnd;
+
+		// Init Direct2D
+		D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+
+		// Get and initialize the default Kinect sensor
+		InitializeDefaultSensor();
+	}
+	break;
+
+	// If the titlebar X is clicked, destroy app
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		break;
+
+	case WM_DESTROY:
+		// Quit the main message pump
+		PostQuitMessage(0);
+		break;
+	}
+
+	return FALSE;
+}
 
 HRESULT SkeletalBasics::InitializeDefaultSensor()
 {
@@ -174,7 +355,7 @@ void SkeletalBasics::Update()
 			else
 			{
 				// The main function to process body data for each frame
-				ProcessBody(BODY_COUNT, ppBodies);
+				ProcessBody(m_nStartTime, BODY_COUNT, ppBodies);
 
 				// Load the used frame data into the "previousframe" pointer to be used in the next frmae for comparison
 				hr = pBodyFrame->GetAndRefreshBodyData(_countof(m_pBodyFromPreviousFrame), m_pBodyFromPreviousFrame);
@@ -191,67 +372,117 @@ void SkeletalBasics::Update()
 
 
 
-void SkeletalBasics::ProcessBody(int nBodyCount, IBody** ppBodies)
+void SkeletalBasics::ProcessBody(INT64 nTime, int nBodyCount, IBody** ppBodies)
 {
-	HRESULT hr;
-
-	// For each single body in bodies list
-	for (int i = 0; i < nBodyCount; ++i)
+	if (m_hWnd)
 	{
-		IBody* pBody = ppBodies[i];
-		IBody* pBodyPrevious = NULL;
-
-		boolean previousBodyLoad = false;
-
-		// If the previous frame data is ready
-		if (m_pBodyFromPreviousFrame[i] != NULL)
+		HRESULT hr;
+		if (SUCCEEDED(hr) && m_pRenderTarget && m_pCoordinateMapper)
 		{
-			previousBodyLoad = true;
-			pBodyPrevious = m_pBodyFromPreviousFrame[i];
+			m_pRenderTarget->BeginDraw();
+			m_pRenderTarget->Clear();
+
+			RECT rct;
+			GetClientRect(GetDlgItem(m_hWnd, IDC_VIDEOVIEW), &rct);
+			int width = rct.right;
+			int height = rct.bottom;
+
+			// For each single body in bodies list
+			for (int i = 0; i < nBodyCount; ++i)
+			{
+				IBody* pBody = ppBodies[i];
+				IBody* pBodyPrevious = NULL;
+
+				boolean previousBodyLoad = false;
+
+				// If the previous frame data is ready
+				if (m_pBodyFromPreviousFrame[i] != NULL)
+				{
+					previousBodyLoad = true;
+					pBodyPrevious = m_pBodyFromPreviousFrame[i];
+				}
+
+				if (pBody)
+				{
+					BOOLEAN bTracked = false;
+					hr = pBody->get_IsTracked(&bTracked);
+
+					// If this body is being tracked
+					if (SUCCEEDED(hr) && bTracked)
+					{
+						Joint joints[JointType_Count];
+
+						D2D1_POINT_2F jointPoints[JointType_Count];
+						HandState leftHandState = HandState_Unknown;
+						HandState rightHandState = HandState_Unknown;
+
+						pBody->get_HandLeftState(&leftHandState);
+						pBody->get_HandRightState(&rightHandState);
+
+
+						hr = pBody->GetJoints(_countof(joints), joints);
+
+						// If joints are obtained successfully, start data process
+						if (SUCCEEDED(hr))
+						{
+
+							// If the data from last frame is loaded, start comparison
+							if (previousBodyLoad)
+							{
+								cout << "Activity Analysis value : " << ActivityAnalysis(pBody, pBodyPrevious) << endl;
+								for (int j = 0; j < _countof(joints); ++j)
+								{
+									jointPoints[j] = BodyToScreen(joints[j].Position, width, height);
+								}
+
+								DrawBody(joints, jointPoints);
+
+								DrawHand(leftHandState, jointPoints[JointType_HandLeft]);
+								DrawHand(rightHandState, jointPoints[JointType_HandRight]);
+							}
+						}
+					}
+				}
+			}
+			hr = m_pRenderTarget->EndDraw();
+			// Device lost, need to recreate the render target
+			// We'll dispose it now and retry drawing
+			if (D2DERR_RECREATE_TARGET == hr)
+			{
+				hr = S_OK;
+				DiscardDirect2DResources();
+			}
 		}
 
-		if (pBody)
+		if (!m_nStartTime)
 		{
-			BOOLEAN bTracked = false;
-			hr = pBody->get_IsTracked(&bTracked);
+			m_nStartTime = nTime;
+		}
 
-			// If this body is being tracked
-			if (SUCCEEDED(hr) && bTracked)
+		double fps = 0.0;
+
+		LARGE_INTEGER qpcNow = { 0 };
+		if (m_fFreq)
+		{
+			if (QueryPerformanceCounter(&qpcNow))
 			{
-				Joint joints[JointType_Count];
-
-				/*
-				Unused code for hand status recognition
-				
-				HandState leftHandState = HandState_Unknown;
-				HandState rightHandState = HandState_Unknown;
-
-				pBody->get_HandLeftState(&leftHandState);
-				pBody->get_HandRightState(&rightHandState);
-				*/
-
-				hr = pBody->GetJoints(_countof(joints), joints);
-
-				// If joints are obtained successfully, start data process
-				if (SUCCEEDED(hr))
+				if (m_nLastCounter)
 				{
-
-					// If the data from last frame is loaded, start comparison
-					if (previousBodyLoad)
-					{
-						cout << "Activity Analysis value : " << ActivityAnalysis(pBody, pBodyPrevious) << endl;
-					}
-
-					// Example to use single jonint data
-					// std::cout << joints[7].Position.Z << std::endl;
-
-					// Example to use all joints in a loop
-					//for (int j = 0; j < _countof(joints); ++j) {}
+					m_nFramesSinceUpdate++;
+					fps = m_fFreq * m_nFramesSinceUpdate / double(qpcNow.QuadPart - m_nLastCounter);
 				}
 			}
 		}
-	}
 
+		WCHAR szStatusMessage[64];
+		StringCchPrintf(szStatusMessage, _countof(szStatusMessage), L" FPS = %0.2f    Time = %I64d", fps, (nTime - m_nStartTime));
+
+		if (SetStatusMessage(szStatusMessage, 1000, false))
+		{
+			m_nLastCounter = qpcNow.QuadPart;
+			m_nFramesSinceUpdate = 0;
+		}
+	}
 }
 
 float SkeletalBasics::ActivityAnalysis(IBody* pBodyFromCurrentFrame, IBody* pBodyFromPreviousFrame)
@@ -368,10 +599,211 @@ void SkeletalBasics::Calibration(IBody** ppBodies)
 	}
 }
 
+bool SkeletalBasics::SetStatusMessage(_In_z_ WCHAR* szMessage, DWORD nShowTimeMsec, bool bForce)
+{
+	INT64 now = GetTickCount64();
+
+	if (m_hWnd && (bForce || (m_nNextStatusTime <= now)))
+	{
+		SetDlgItemText(m_hWnd, IDC_STATUS, szMessage);
+		m_nNextStatusTime = now + nShowTimeMsec;
+
+		return true;
+	}
+
+	return false;
+}
+
+HRESULT SkeletalBasics::EnsureDirect2DResources()
+{
+	HRESULT hr = S_OK;
+
+	if (m_pD2DFactory && !m_pRenderTarget)
+	{
+		RECT rc;
+		GetWindowRect(GetDlgItem(m_hWnd, IDC_VIDEOVIEW), &rc);
+
+		int width = rc.right - rc.left;
+		int height = rc.bottom - rc.top;
+		D2D1_SIZE_U size = D2D1::SizeU(width, height);
+		D2D1_RENDER_TARGET_PROPERTIES rtProps = D2D1::RenderTargetProperties();
+		rtProps.pixelFormat = D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE);
+		rtProps.usage = D2D1_RENDER_TARGET_USAGE_GDI_COMPATIBLE;
+
+		// Create a Hwnd render target, in order to render to the window set in initialize
+		hr = m_pD2DFactory->CreateHwndRenderTarget(
+			rtProps,
+			D2D1::HwndRenderTargetProperties(GetDlgItem(m_hWnd, IDC_VIDEOVIEW), size),
+			&m_pRenderTarget
+		);
+
+		if (FAILED(hr))
+		{
+			SetStatusMessage(L"Couldn't create Direct2D render target!", 10000, true);
+			return hr;
+		}
+
+		// light green
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(0.27f, 0.75f, 0.27f), &m_pBrushJointTracked);
+
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Yellow, 1.0f), &m_pBrushJointInferred);
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Green, 1.0f), &m_pBrushBoneTracked);
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Gray, 1.0f), &m_pBrushBoneInferred);
+
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Red, 0.5f), &m_pBrushHandClosed);
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Green, 0.5f), &m_pBrushHandOpen);
+		m_pRenderTarget->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::Blue, 0.5f), &m_pBrushHandLasso);
+	}
+
+	return hr;
+}
+
+void SkeletalBasics::DiscardDirect2DResources()
+{
+	SafeRelease(m_pRenderTarget);
+
+	SafeRelease(m_pBrushJointTracked);
+	SafeRelease(m_pBrushJointInferred);
+	SafeRelease(m_pBrushBoneTracked);
+	SafeRelease(m_pBrushBoneInferred);
+
+	SafeRelease(m_pBrushHandClosed);
+	SafeRelease(m_pBrushHandOpen);
+	SafeRelease(m_pBrushHandLasso);
+}
+
+D2D1_POINT_2F SkeletalBasics::BodyToScreen(const CameraSpacePoint& bodyPoint, int width, int height)
+{
+	// Calculate the body's position on the screen
+	DepthSpacePoint depthPoint = { 0 };
+	m_pCoordinateMapper->MapCameraPointToDepthSpace(bodyPoint, &depthPoint);
+
+	float screenPointX = static_cast<float>(depthPoint.X * width) / cDepthWidth;
+	float screenPointY = static_cast<float>(depthPoint.Y * height) / cDepthHeight;
+
+	return D2D1::Point2F(screenPointX, screenPointY);
+}
+
+void SkeletalBasics::DrawBody(const Joint* pJoints, const D2D1_POINT_2F* pJointPoints)
+{
+	// Draw the bones
+
+	// Torso
+	DrawBone(pJoints, pJointPoints, JointType_Head, JointType_Neck);
+	DrawBone(pJoints, pJointPoints, JointType_Neck, JointType_SpineShoulder);
+	DrawBone(pJoints, pJointPoints, JointType_SpineShoulder, JointType_SpineMid);
+	DrawBone(pJoints, pJointPoints, JointType_SpineMid, JointType_SpineBase);
+	DrawBone(pJoints, pJointPoints, JointType_SpineShoulder, JointType_ShoulderRight);
+	DrawBone(pJoints, pJointPoints, JointType_SpineShoulder, JointType_ShoulderLeft);
+	DrawBone(pJoints, pJointPoints, JointType_SpineBase, JointType_HipRight);
+	DrawBone(pJoints, pJointPoints, JointType_SpineBase, JointType_HipLeft);
+
+	// Right Arm    
+	DrawBone(pJoints, pJointPoints, JointType_ShoulderRight, JointType_ElbowRight);
+	DrawBone(pJoints, pJointPoints, JointType_ElbowRight, JointType_WristRight);
+	DrawBone(pJoints, pJointPoints, JointType_WristRight, JointType_HandRight);
+	DrawBone(pJoints, pJointPoints, JointType_HandRight, JointType_HandTipRight);
+	DrawBone(pJoints, pJointPoints, JointType_WristRight, JointType_ThumbRight);
+
+	// Left Arm
+	DrawBone(pJoints, pJointPoints, JointType_ShoulderLeft, JointType_ElbowLeft);
+	DrawBone(pJoints, pJointPoints, JointType_ElbowLeft, JointType_WristLeft);
+	DrawBone(pJoints, pJointPoints, JointType_WristLeft, JointType_HandLeft);
+	DrawBone(pJoints, pJointPoints, JointType_HandLeft, JointType_HandTipLeft);
+	DrawBone(pJoints, pJointPoints, JointType_WristLeft, JointType_ThumbLeft);
+
+	// Right Leg
+	DrawBone(pJoints, pJointPoints, JointType_HipRight, JointType_KneeRight);
+	DrawBone(pJoints, pJointPoints, JointType_KneeRight, JointType_AnkleRight);
+	DrawBone(pJoints, pJointPoints, JointType_AnkleRight, JointType_FootRight);
+
+	// Left Leg
+	DrawBone(pJoints, pJointPoints, JointType_HipLeft, JointType_KneeLeft);
+	DrawBone(pJoints, pJointPoints, JointType_KneeLeft, JointType_AnkleLeft);
+	DrawBone(pJoints, pJointPoints, JointType_AnkleLeft, JointType_FootLeft);
+
+	// Draw the joints
+	for (int i = 0; i < JointType_Count; ++i)
+	{
+		D2D1_ELLIPSE ellipse = D2D1::Ellipse(pJointPoints[i], c_JointThickness, c_JointThickness);
+
+		if (pJoints[i].TrackingState == TrackingState_Inferred)
+		{
+			m_pRenderTarget->FillEllipse(ellipse, m_pBrushJointInferred);
+		}
+		else if (pJoints[i].TrackingState == TrackingState_Tracked)
+		{
+			m_pRenderTarget->FillEllipse(ellipse, m_pBrushJointTracked);
+		}
+	}
+}
+
+void SkeletalBasics::DrawBone(const Joint* pJoints, const D2D1_POINT_2F* pJointPoints, JointType joint0, JointType joint1)
+{
+	TrackingState joint0State = pJoints[joint0].TrackingState;
+	TrackingState joint1State = pJoints[joint1].TrackingState;
+
+	// If we can't find either of these joints, exit
+	if ((joint0State == TrackingState_NotTracked) || (joint1State == TrackingState_NotTracked))
+	{
+		return;
+	}
+
+	// Don't draw if both points are inferred
+	if ((joint0State == TrackingState_Inferred) && (joint1State == TrackingState_Inferred))
+	{
+		return;
+	}
+
+	// We assume all drawn bones are inferred unless BOTH joints are tracked
+	if ((joint0State == TrackingState_Tracked) && (joint1State == TrackingState_Tracked))
+	{
+		m_pRenderTarget->DrawLine(pJointPoints[joint0], pJointPoints[joint1], m_pBrushBoneTracked, c_TrackedBoneThickness);
+	}
+	else
+	{
+		m_pRenderTarget->DrawLine(pJointPoints[joint0], pJointPoints[joint1], m_pBrushBoneInferred, c_InferredBoneThickness);
+	}
+}
+
+void SkeletalBasics::DrawHand(HandState handState, const D2D1_POINT_2F& handPosition)
+{
+	D2D1_ELLIPSE ellipse = D2D1::Ellipse(handPosition, c_HandSize, c_HandSize);
+
+	switch (handState)
+	{
+	case HandState_Closed:
+		m_pRenderTarget->FillEllipse(ellipse, m_pBrushHandClosed);
+		break;
+
+	case HandState_Open:
+		m_pRenderTarget->FillEllipse(ellipse, m_pBrushHandOpen);
+		break;
+
+	case HandState_Lasso:
+		m_pRenderTarget->FillEllipse(ellipse, m_pBrushHandLasso);
+		break;
+	}
+}
 
 
+/*
 int main()
 {
 	SkeletalBasics application;
 	application.Run();
+}
+*/
+int APIENTRY wWinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nShowCmd
+)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	SkeletalBasics application;
+	application.Run(hInstance, nShowCmd);
 }
