6e9d01a864080edc92dac6209e29c6115c8331d1
Jan <jan.kolarik.18@ucl.ac.uk>
2020-02-25 15:27:33 +0000

    Final Structure

---
+++ Apple Watch Code/DanceHeatlhTracking/DanceHeatlhTracking WatchKit Extension/StartWorkout.swift
@@ -0,0 +1,9 @@
+//
+//  StartWorkout.swift
+//  DanceHeatlhTracking WatchKit Extension
+//
+//  Created by Honza on 25/02/2020.
+//  Copyright © 2020 Team32. All rights reserved.
+//
+
+import Foundation
