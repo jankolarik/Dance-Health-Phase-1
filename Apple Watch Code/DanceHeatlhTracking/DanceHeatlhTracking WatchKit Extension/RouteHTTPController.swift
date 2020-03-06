e0ae132b8f2d39c8ca19f52bea0b426d8c862198
Jan <jan.kolarik.18@ucl.ac.uk>
2020-03-06 10:01:47 +0000

    Added new titles according to grading criteria

--- Apple Watch Code/DanceHeatlhTracking/DanceHeatlhTracking WatchKit Extension/RouteHTTPController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RouteHTTPController.swift
-//  DanceHeatlhTracking WatchKit Extension
-//
-//  Created by Honza on 09/02/2020.
-//  Copyright © 2020 Team32. All rights reserved.
-//
-
-import Foundation
